917df4317eead4fd4ba27b34ae6397003f96680e
Allewe <badraliyoumaiga199@gmail.com>
2022-06-07 19:11:00 +0100

    fixed build bug commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
     images: {
         domains: ["black-industry.s3.eu-west-3.amazonaws.com"],
     },
