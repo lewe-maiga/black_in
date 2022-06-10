9b478b1055b314a7806c8f94c89f685568a4bd44
Allewe <badraliyoumaiga199@gmail.com>
2022-06-10 23:17:56 +0100

    fixed seo rules

--- components/layout/index.tsx
+++ components/layout/index.tsx
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import { ReactChild, ReactChildren } from "react";
 import globalStyles from "@styles/globals";
+import { useRouter } from "next/router";
 
 type LayoutProps = {
 	children: ReactChild | ReactChildren;
@@ -9,13 +10,21 @@ type LayoutProps = {
 };
 
 const Layout = ({ title, description, children }: LayoutProps) => {
+	const router = useRouter();
+	console.log(router);
+
 	return (
 		<>
 			<Head>
 				<title>{title}</title>
+				<link rel="canonical" href={`https://black-in.vercel.app${router.asPath}`} />
+				<link rel="shortcut icon" href="/icon.ico" type="image/x-icon" />
 				<meta name="viewport" content="width=device-width, initial-scale=1" />
 				<meta name="description" content={description} />
-				<link rel="shortcut icon" href="/icon.ico" type="image/x-icon" />
+				<meta property="og:title" content={title} />
+				<meta property="og:description" content={description} />
+				<meta property="og:url" content="https://black-in.vercel.app/" />
+				<meta property="og:type" content="website" />
 			</Head>
 
 			<div className="content">{children}</div>
--- pages/admin/register.tsx
+++ pages/admin/register.tsx
@@ -1,47 +1,58 @@
-
-import {Login} from "components/admin/login";
-import {useEffect} from "react";
-import {useToken} from "@lib/hooks";
-import {fetcher} from "@lib/utils";
+import { Login } from "components/admin/login";
+import { ReactElement, useEffect } from "react";
+import { useToken } from "@lib/hooks";
+import { fetcher } from "@lib/utils";
 import useSWR from "swr";
-import {useRouter} from "next/router";
-import {Spinner} from "@components/spinner";
+import { useRouter } from "next/router";
+import { Spinner } from "@components/spinner";
+import Head from "next/head";
 
 const Register = () => {
-    const router = useRouter();
-    const {token, saveToken, _id} = useToken();
-    const {data, error} = useSWR("/api/admin/count", fetcher, {});
+	const router = useRouter();
+	const { token, saveToken, _id } = useToken();
+	const { data, error } = useSWR("/api/admin/count", fetcher, {});
 
-    useEffect(() => {
-        if (token && _id) router.push(`/admin/dashboard`);
-    }, [token, router, _id]);
-    if (!data)
-        return (
-            <div>
-                <Spinner />
-            </div>
-        );
-    if (error) return error;
-    return (
-        <>
-            <div className="container">
-                <Login setToken={saveToken} router={router} />
-            </div>
+	useEffect(() => {
+		if (token && _id) router.push(`/admin/dashboard`);
+	}, [token, router, _id]);
+	if (!data)
+		return (
+			<div>
+				<Spinner />
+			</div>
+		);
+	if (error) return error;
+	return (
+		<>
+			<div className="container">
+				<Login setToken={saveToken} router={router} />
+			</div>
 
-            <style jsx>
-                {`
-                    div.container {
-                        display: flex;
-                        width: 100vw;
-                        height: 100vh;
-                        justify-content: center;
-                        align-items: center;
-                        padding: 40px;
-                    }
-                `}
-            </style>
-        </>
-    );
+			<style jsx>
+				{`
+					div.container {
+						display: flex;
+						width: 100vw;
+						height: 100vh;
+						justify-content: center;
+						align-items: center;
+						padding: 40px;
+					}
+				`}
+			</style>
+		</>
+	);
+};
+
+Register.getLayout = function getLayout(page: ReactElement) {
+	return (
+		<>
+			<Head>
+				<meta name="robots" content="noindex,nofollow" />
+			</Head>
+			{page}
+		</>
+	);
 };
 
 export default Register;
